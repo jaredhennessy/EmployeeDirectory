bccd9e48613efab5755f1a82ae69bbd383f84d18
jaredhennessy <jared.hennessy@gmail.com>
2020-07-16 17:47:57 -0700

    configured reacto / axios

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Header from "./components/Header";
+import Employees from "./components/Employees/Employees";
+
+function App() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Employees />
+    </div>
+  );
+}
+
+export default App;
