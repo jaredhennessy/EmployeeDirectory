9ee539dc8aae9fccafd60802142a85f582f42130
jaredhennessy <jared.hennessy@gmail.com>
2020-07-17 17:34:28 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import PageHeader from "./components/PageHeader";
 import Search from "./components/Search";
 import Employees from "./components/Employees";
-import API from "./utils/API.js";
+import API from "./utils/api";
 
 class App extends React.Component {
   state = {
